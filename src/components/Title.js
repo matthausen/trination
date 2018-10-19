3c35fc364405daa3655b4ae1026db38de68c1b65
Matteo <matteo@Matteos-MacBook-Pro.local>
2018-10-19 10:58:09 +0100

    Server built

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = (props) => (
     <div className="header container">
         <h1 className="title">TRINATION</h1>
-        <img src="/vintageFootball.png" alt="trination-logo" className="vintage"/>
+        <img src="./vintageFootball.png" alt="trination-logo" className="vintage"/>
     </div>
 );
 
