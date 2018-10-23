aac0df741a0aeae8ee795ecca21cbbfd1c0cc38e
Matteo <matthausen@Matteos-MacBook-Air.local>
2018-10-23 23:30:30 +0100

    updated general layout

--- src/App.js
+++ src/App.js
@@ -36,18 +36,20 @@ class App extends Component {
     return (
       <div className="App">
       <Title/>
+      <div className="jumbotron">
         <div className="block">
           <h3>Player 1: </h3>
           <h2>{this.state.randomCountry1}</h2>
-          <h1 className="inline-block">{this.state.player1}</h1>
+          <h2 className="team">{this.state.player1}</h2>
         </div>
         <div className="block">
           <h3>Player 2: </h3>
           <h2>{this.state.randomCountry2}</h2>
-          <h1>{this.state.player2}</h1>
+          <h2 className="team">{this.state.player2}</h2>
         </div>
         <div>
           <button onClick={this.shuffleGame} className="btn btn-info shuffle">Shuffle</button>
+        </div>
         </div>
       </div>
     );
